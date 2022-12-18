173f83b1d8cec8d9f9f58116f9e48f0ac014d56e
FravinCustodianStudent <3114226@knu.ua>
2022-12-18 16:27:25 +0200

    Initial commit

---
+++ src/main/java/spring/Introduction/Dog.java
@@ -0,0 +1,8 @@
+package spring.Introduction;
+
+public class Dog implements Pet {
+    public void say()
+    {
+        System.out.println("Bow-Wow");
+    }
+}
---
+++ src/main/java/spring/Introduction/Pet.java
@@ -0,0 +1,5 @@
+package spring.Introduction;
+
+public interface Pet {
+    public void say();
+}
